cf135f75503e251af41380ca73b2124c25dafa8d
Davi Cardoso <davicardoso524@academico.ufs.br>
2025-07-13 21:39:44 -0300

    Criação da rota pedido, que vai basicamente Criar o pedido, que recebe o id do Usuario passado pelo Schema, e ele retorna se o pedido foi criado com sucesso e o id do pedido

--- routes/orders_routes.py
+++ routes/orders_routes.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from dependencies.db import pegar_sessao
+from schemas.pedido import PedidoSchema
+from models.models import Pedido
+
 
 order_router = APIRouter(prefix='/orders', tags=['Orders'])
 
@@ -8,3 +13,10 @@ async def orders():
     Essa é a rota padrão de pedidos do nosso sistema. Todas as rotas dos pedidos precisam de autenticação
     """
     return {'message': 'Você acessou a rota de pedidos'}
+
+@order_router.post('/pedido')
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
+    novo_pedido = Pedido(usuario=pedido_schema.usuario)
+    session.add(novo_pedido)
+    session.commit()
+    return {"mensagem": f"Pedido criado com sucesso. ID do Pedido: {novo_pedido.id}"}

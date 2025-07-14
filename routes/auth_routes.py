64c945ec65c636fe0049b76d8525d7bb51b6b5aa
Davi Cardoso <davicardoso524@academico.ufs.br>
2025-07-13 23:37:57 -0300

    Fizemos a criação das rotas de LOGIN, REFRESH, que por meio delas é possivel agora gerar o access_token que traz a segurança do JWT pros endpoints, por meio das funçoes autenticar_usuario e criar_token, e algumas dependecias. E a rota LOGIN-FORM que serve para que seja possivel usar a available authorization no swagger da api

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -2,11 +2,33 @@ from fastapi import APIRouter, Depends, HTTPException
 from models.models import Usuario
 from sqlalchemy.orm import Session
 from dependencies.db import pegar_sessao
-from app.main import bcrypt_context
+from dependencies.token import verificar_token
+from app.main import bcrypt_context, ALGORITHM, ACESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas.usuario import UsuarioSchema
+from schemas.login import LoginSchema
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 auth_router = APIRouter(prefix='/auth', tags=['Auth'])
+
+def criar_token(id_usuario, duracao_token=timedelta(minutes=ACESS_TOKEN_EXPIRE_MINUTES)):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {"sub": str(id_usuario), "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
+
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if not usuario:
+        return False
+    
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False 
+    
+    return usuario 
+
 
 @auth_router.get('/')
 async def home():
@@ -36,3 +58,41 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
         session.commit()
         
         return {"mensagem": f"Usuário cadastrado com sucesso {usuario_schema.email}"}
+    
+# login -> email e senha -> token JWT (JSON WEB TOKEN)
+@auth_router.post('/login')
+async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais inválidas")
+    
+    else: 
+        access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "Bearer"
+        }
+    
+@auth_router.post('/login-form')
+async def login_form(dados_formulario: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(pegar_sessao)):
+    usuario = autenticar_usuario(dados_formulario.username, dados_formulario.password, session)
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais inválidas")
+    
+    else: 
+        access_token = criar_token(usuario.id)
+        return {
+            "access_token": access_token,
+            "token_type": "Bearer"
+        }
+    
+@auth_router.post('/refresh')
+async def use_refresh_token(usuario: Usuario = Depends(verificar_token)):
+    access_token = criar_token(usuario.id)
+    
+    return {
+            "access_token": access_token,   
+            "token_type": "Bearer"
+        }

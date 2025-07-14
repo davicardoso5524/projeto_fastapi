18d9968671417444112f240a14878f9a890ada46
Davi Cardoso <davicardoso524@academico.ufs.br>
2025-07-13 22:56:27 -0300

    Adição da variavel de ambiente do  tipo do Algoritmo de criptografia pro JWT, e variavel de ambiente para o tempo de expiração do access token, junto com o schema oauth2, para validação de rotas por meio de header

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
 import os
 
@@ -7,10 +8,13 @@ import os
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACESS_TOKEN_EXPIRE_MINUTES"))
 
 app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login-form")
 
 from routes.auth_routes import auth_router
 from routes.orders_routes import order_router

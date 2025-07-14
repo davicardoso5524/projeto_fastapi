045b055ceec5f423fa994001a975fede89913dda
Davi Cardoso <davicardoso524@academico.ufs.br>
2025-07-13 23:26:55 -0300

    Criação da dependência que faz a verificação dos tokens, resumindo essa dependecia é a que faz o bloqueio dos endpoints, pois todo endpoint que possuir essa dependência irá ser necessário passar o bearer token pelo header

---
+++ dependencies/token.py
@@ -0,0 +1,20 @@
+from fastapi import Depends, HTTPException
+from app.main import SECRET_KEY, ALGORITHM, oauth2_schema
+from dependencies.db import pegar_sessao
+from models.models import Usuario
+from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+
+
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_usuario = int(dic_info.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Acesso Negado, verifique a validade do token")
+    
+    usuario = session.query(Usuario).filter(Usuario.id==id_usuario).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso Inválido")
+    return usuario
+
